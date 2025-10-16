a60a338407a92a38bf89a6442769723395a27b7c
Homán Dániel <daniel1.homan@gmail.com>
2025-10-16 18:48:28 +0200

    Első commit

---
+++ lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:quiz_app/start_screen.dart';
+
+void main() {
+  runApp(const MainApp());
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Color.fromARGB(255, 63, 235, 123),
+                Color.fromARGB(255, 9, 123, 122),
+              ],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            ),
+          ),
+          child: StartScreen(),
+        ),
+      ),
+    );
+  }
+}
