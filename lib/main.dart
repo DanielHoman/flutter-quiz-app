d765a7c00496fbf917b57c545f50e9716fcb918e
Homán Dániel <daniel1.homan@gmail.com>
2025-10-16 22:07:30 +0200

    Refactor app structure: replace MainApp with Quiz, implement screen switching

--- lib/main.dart
+++ lib/main.dart
@@ -1,31 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:quiz_app/start_screen.dart';
+import 'package:quiz_app/quiz.dart';
 
 void main() {
-  runApp(const MainApp());
-}
-
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        body: Container(
-          decoration: const BoxDecoration(
-            gradient: LinearGradient(
-              colors: [
-                Color.fromARGB(255, 63, 235, 123),
-                Color.fromARGB(255, 9, 123, 122),
-              ],
-              begin: Alignment.topLeft,
-              end: Alignment.bottomRight,
-            ),
-          ),
-          child: StartScreen(),
-        ),
-      ),
-    );
-  }
+  runApp(const Quiz());
 }
